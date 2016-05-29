ad5d24d061a8d6b0d2c2e1d2680fbc0b82849815
Roman Gordeev <opportunato@MacBook-Pro-Mac.local>
2016-05-29 23:18:17 +0300

    Added basic slider

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,14 @@ const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
 const del = require('del');
-const concat = require('gulp-concat');
+const concatCss = require('gulp-concat');
 const nunjucksRender = require('gulp-nunjucks-render');
 const data = require('gulp-data');
+const uglify = require('gulp-uglify');
+
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const browserify = require('browserify');
 
 const postcss = require('gulp-postcss');
 
@@ -21,13 +26,31 @@ const variables = {
 gulp.task('styles', function() {
   return gulp.src('frontend/styles/*.css')
     .pipe(gulpIf(isDevelopment, sourcemaps.init()))
-    .pipe(concat("main.css"))
     .pipe(postcss([
+      require('postcss-nested'),
       require('autoprefixer')({ browsers: ['last 3 versions'] }),
       require('postcss-simple-vars')({ variables: variables })
     ]))
+    .pipe(concatCss("main.css"))
     .pipe(gulpIf(isDevelopment, sourcemaps.write()))
     .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('scripts', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './frontend/js/index.js',
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/js/'));
 });
 
 gulp.task('clean', function() {
@@ -53,12 +76,13 @@ gulp.task('nunjucks', function() {
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'assets', 'nunjucks')));
+gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'assets', 'nunjucks', 'scripts')));
 
 gulp.task('watch', function() {
   gulp.watch('frontend/styles/**/*.*', gulp.series('styles'));
   gulp.watch('frontend/assets/**/*.*', gulp.series('assets'));
   gulp.watch('frontend/data/**/*.*', gulp.series('nunjucks'));
+  gulp.watch('frontend/js/**/*.*', gulp.series('scripts'));
   gulp.watch('frontend/(pages|templates)/**/*.*', gulp.series('nunjucks'));
 });
 
